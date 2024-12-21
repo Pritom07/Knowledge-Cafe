5bfb74e711ddc0abd61e10babea3c3080adbd541
Pritom07 <pritommohajon4545@gmail.com>
2024-12-21 22:32:05 +0600

    header section complite

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div className="flex justify-between items-center border-slate-300 border-b-2 max-w-6xl mx-auto mt-1">
+            <h1 className="text-4xl font-bold">KNOWLEDGE CAFE</h1>
+            <img className="mb-1" src="../../../Images/profile.png" />
+        </div>
+    );
+};
+
+export default Header;
