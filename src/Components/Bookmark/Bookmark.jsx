3facd5f2807d45e38194692deb4fa6a542515862
Pritom07 <pritommohajon4545@gmail.com>
2024-12-23 13:21:38 +0600

    titles are successfully added with styles

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 const Bookmark = ({ titles }) => {
     return (
         <div className="w-1/3">
-            <div>
-                <h1>BookMarked Blogs : {titles.length}</h1>
+            <div className='mt-10 bg-slate-300 rounded-xl ml-6 p-5'>
+                <h1 className='text-2xl font-bold'>Bookmarked Blogs : {titles.length}</h1>
                 {
-                    titles.map(blog => <p key={blog.id}> {blog.title} </p>)
+                    titles.map(blog => <p key={blog.id} className='text-xl font-medium bg-slate-100 rounded-lg mt-2 p-3'> {blog.title} </p>)
                 }
             </div>
         </div>
